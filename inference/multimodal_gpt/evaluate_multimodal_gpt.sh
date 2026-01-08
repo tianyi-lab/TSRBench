89d33ab6a634db01d5c748b87fccfbbfb10e742a
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2026-01-08 17:17:04 +0800

    Update evaluate_multimodal_gpt.sh

--- inference/multimodal_gpt/evaluate_multimodal_gpt.sh
+++ inference/multimodal_gpt/evaluate_multimodal_gpt.sh
@@ -4,29 +4,25 @@ o4-mini
 gpt-5-mini
 )
 
-
-
 DATASETS=(
   ./dataset/perception/perception.jsonl
   ./dataset/reasoning/temporal_relation_reasoning/temporal_relation_reasoning.jsonl
   ./dataset/reasoning/inductive_reasoning/inductive_reasoning.jsonl
-  ./dataset/reasoning/math_reasoning/math_reasoning.jsonl
+  ./dataset/reasoning/numerical_reasoning/numerical_reasoning.jsonl
   ./dataset/reasoning/causal_reasoning/causal_reasoning.jsonl
   ./dataset/reasoning/etiological_reasoning/etiological_reasoning.jsonl
-  ./dataset/reasoning/abductive_reasoning/abductive_reasoning.json
+  ./dataset/reasoning/abductive_reasoning/abductive_reasoning.jsonl
   ./dataset/reasoning/deductive_reasoning/deductive_reasoning.jsonl
-  ./dataset/prediction/time_series_forecasting/time_series_forecasting.jsonl
-  ./dataset/prediction/event_forecast/event_forecast.jsonl
-  ./dataset/decision/pattern_decision/pattern_decision.jsonl
-  ./dataset/decision/quantitative_decision/quantitative_decision.jsonl
+  ./dataset/prediction/time_series_forecasting.jsonl
+  ./dataset/prediction/event_prediction.jsonl
+  ./dataset/decision/qualitative_decision.jsonl
+  ./dataset/decision/quantitative_decision.jsonl
 )
 WORKDIR="./evaluation"
 
 # OPENAI
 OPENAI_BASE_URL=$1
 OPENAI_API_KEY=$2
-
-
 
 WORKDIR="./evaluation"
 NUM_WORKERS=64
